ec6f5e9ff86470e2b4f2037c67d381b869ed88a6
AJ Lera <you@example.com>
2024-05-18 00:46:55 +0800

    Fixed the Job model

--- model/jobModel.js
+++ model/jobModel.js
@@ -10,23 +10,40 @@ const jobSchema = new mongoose.Schema({
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
+    required: true,
   },
   details: {
-    why: {
-      title: String,
-      description: String,
+    descriptions: {
+      why: {
+        type: String,
+        required: true,
+      },
+      what: {
+        type: String,
+        required: true,
+      },
     },
-    what: {
-      title: String,
-      description: String,
-    },
-    responsibilities: {
-      title: String,
-      lists: [String],
-    },
-    requirements: {
-      title: String,
-      lists: [String],
+    lists: {
+      responsibilities: {
+        type: [String],
+        required: true,
+        validate: {
+          validator: function (array) {
+            return array.length > 0;
+          },
+          message: "At least one responsibility is required.",
+        },
+      },
+      requirements: {
+        type: [String],
+        required: true,
+        validate: {
+          validator: function (array) {
+            return array.length > 0;
+          },
+          message: "At least one requirement is required.",
+        },
+      },
     },
   },
 });
