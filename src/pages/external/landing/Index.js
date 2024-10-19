997f1d384434680a87bcc2ef9a4b5aa02ee5c9a3
AJ Lera <you@example.com>
2024-10-19 15:41:31 +0800

    removed a proxy

--- src/pages/external/landing/Index.js
+++ src/pages/external/landing/Index.js
@@ -9,21 +9,6 @@ import Contact from "./contact/Contact";
 import Footer from "../../../components/shared/Footer";
 import { useEffect } from "react";
 const Landing = () => {
-  const fetchTest = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      sameSite: "None",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Fetch error:", err));
-  };
-  useEffect(() => {
-    fetchTest();
-  }, []);
-
   return (
     <>
       <Navbar />
