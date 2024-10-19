a705668c8e0563d22bca42a09ec5024666b39171
AJ Lera <you@example.com>
2024-10-19 16:15:31 +0800

    fix env 3

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: [APP, RENDER],
+    origin: [APP, RENDER, "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
