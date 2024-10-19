866b6511d53996a8941e2867688d698cc382c996
AJ Lera <you@example.com>
2024-10-19 18:10:52 +0800

    fixing live server 5

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use("/api", chatRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: APP,
     methods: ["GET", "POST"],
     credentials: true,
   },
