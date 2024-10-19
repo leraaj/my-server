534ebe3a7c44ba303a5f12904f4ce44b58f71a0b
AJ Lera <you@example.com>
2024-10-19 16:05:31 +0800

    fix env 1

--- index.js
+++ index.js
@@ -38,10 +38,12 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: [BASE, APP, RENDER, "*"],
+    origin: [APP, RENDER],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 // ROUTES
 app.use("/api", userRoute);
 app.use("/api", jobRoute);
@@ -54,7 +56,7 @@ app.use("/api", chatRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [BASE, APP, RENDER, "*"],
+    origin: [APP, RENDER],
     methods: ["GET", "POST"],
     credentials: true,
   },
